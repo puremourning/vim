57da07217a39ed2120e4cd016f118553f79eb15b
Ben Jackson <puremourning@gmail.com>
2020-03-14 11:42:21 +0000

    We can now move to a full debug_backtrace_level-based approach and use estack everywhere. update comments

--- src/evalfunc.c
+++ src/evalfunc.c
@@ -3745,7 +3745,7 @@ f_debug_getvariables(typval_T *argvars, typval_T *rettv)
 	emsg("Invalid stack level");
 	return;
     }
-    entry = estack_get_level(stack_level);
+    entry = estack_get_level(stack_level); // TODO: Inline
 
     // FIXME: I just found get_funccal() in userfunc.c, which could mean we
     // don't need to store funccals in the estack at all
@@ -3765,6 +3765,8 @@ f_debug_getvariables(typval_T *argvars, typval_T *rettv)
     // complete. I can see why the backtrace is only functions, because that
     // means always one sctx - i would need to extend the backtracing to include
     // current_sctx as well.
+    //
+    // Now that the estack contains scid_T, we can do this.
 
     if ( (*scope == 'l' || *scope == 'a') && entry->es_type != ETYPE_UFUNC) {
 	emsg("No function scope for this stack frame");
