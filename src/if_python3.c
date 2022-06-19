6602385b41ba07a54a0c779cc02e4c292bc8cd36
Ben Jackson <puremourning@gmail.com>
2022-06-19 12:27:34 +0100

    Add locals argument to py3eval

--- src/if_python3.c
+++ src/if_python3.c
@@ -1220,7 +1220,11 @@ fail:
  * External interface
  */
     static void
-DoPyCommand(const char *cmd, rangeinitializer init_range, runner run, void *arg)
+DoPyCommand(const char *cmd,
+	    dict_T* locals,
+	    rangeinitializer init_range,
+	    runner run,
+	    void *arg)
 {
 #if defined(HAVE_LOCALE_H) || defined(X_LOCALE)
     char		*saved_locale;
@@ -1261,7 +1265,7 @@ DoPyCommand(const char *cmd, rangeinitializer init_range, runner run, void *arg)
     cmdbytes = PyUnicode_AsEncodedString(cmdstr, "utf-8", ERRORS_ENCODE_ARG);
     Py_XDECREF(cmdstr);
 
-    run(PyBytes_AsString(cmdbytes), arg, &pygilstate);
+    run(PyBytes_AsString(cmdbytes), locals, arg, &pygilstate);
     Py_XDECREF(cmdbytes);
 
     PyGILState_Release(pygilstate);
@@ -1296,6 +1300,7 @@ ex_py3(exarg_T *eap)
 	    p_pyx = 3;
 
 	DoPyCommand(script == NULL ? (char *) eap->arg : (char *) script,
+		    NULL,
 		(rangeinitializer) init_range_cmd,
 		(runner) run_cmd,
 		(void *) eap);
@@ -1362,6 +1367,7 @@ ex_py3file(exarg_T *eap)
 
     // Execute the file
     DoPyCommand(buffer,
+		NULL,
 	    (rangeinitializer) init_range_cmd,
 	    (runner) run_cmd,
 	    (void *) eap);
@@ -1374,6 +1380,7 @@ ex_py3do(exarg_T *eap)
 	p_pyx = 3;
 
     DoPyCommand((char *)eap->arg,
+		NULL,
 	    (rangeinitializer)init_range_cmd,
 	    (runner)run_do,
 	    (void *)eap);
@@ -1921,9 +1928,10 @@ LineToString(const char *str)
 }
 
     void
-do_py3eval(char_u *str, typval_T *rettv)
+do_py3eval(char_u *str, dict_T * locals, typval_T *rettv)
 {
     DoPyCommand((char *) str,
+		locals,
 	    (rangeinitializer) init_range_eval,
 	    (runner) run_eval,
 	    (void *) rettv);
